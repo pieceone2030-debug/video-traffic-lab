25ef86a98bc9cec7cf2751c76d157569f498ae95
pieceone2030-debug <pieceone2030@gmail.com>
2026-09-21 10:59:40 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,7 +28,7 @@ chromiumExtra.use(AnonymizeUA());
    ═══════════════════════════════════════════════════════ */
 
 const CFG = {
-    targetUrl:          process.env.TARGET_URL          || "https://www.tiktok.com/@candyblast44/video/7671386366139239687/",
+    targetUrl:          process.env.TARGET_URL          || "https://www.youtube.com/shorts/7ZPNbUrfta0/",
     botId:              process.env.BOT_ID              || "1",
     maxDuration:        parseInt(process.env.MAX_DURATION_MINUTES || "8", 10),
     adBlockerRate:      parseFloat(process.env.ADBLOCKER_RATE || "0.20"),
